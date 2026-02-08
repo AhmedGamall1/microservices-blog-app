e2e57998881626d4718b57f336807f220579676d
ahmed <ag3204123@gmail.com>
2026-02-08 13:47:36 -0800

    emit comments events

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -11,7 +14,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -20,6 +23,15 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:8005/events", {
+    type: "CommentCreated",
+    data: {
+      id,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });

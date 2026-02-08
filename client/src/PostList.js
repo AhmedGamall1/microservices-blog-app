4dde34edbf6d810999c904d4424fd340c316c664
ahmed <ag3204123@gmail.com>
2026-02-08 07:47:11 -0800

    add post list

---
+++ client/src/PostList.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const PostList = () => {
+  const [posts, setPosts] = useState({});
+  const fetchPosts = async () => {
+    const res = await axios.get("http://localhost:8000/posts");
+
+    setPosts(res.data);
+  };
+
+  // run this function one time
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map((post) => (
+    <div
+      style={{ width: "30%", marginBottom: "20px" }}
+      className="card"
+      key={post.title}
+    >
+      <div className="card-body">
+        <h3>{post.title}</h3>
+      </div>
+    </div>
+  ));
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
+};
+
+export default PostList;
